ee98433a1e5780f231be5b666721ebcc5202a2f9
Orson Peters <orsonpeters@gmail.com>
2021-09-12 22:55:07 +0200

    Working initial version.

--- src/lib.rs
+++ src/lib.rs
@@ -3,55 +3,70 @@ use core::convert::TryInto;
 
 #[derive(Copy, Clone, Debug, Hash)]
 #[repr(transparent)]
-struct RealOrd<T>(T);
+pub struct NumOrd<T>(pub T);
 
-macro_rules! signed_float_impl_body {
-    ($IntT:ty, $FloatT:ty, $lhs:expr, $rhs:expr, $op:ident, $less:expr, $greater:expr, $nan:expr) => {
-        if core::mem::size_of::<$IntT>() < core::mem::size_of::<$FloatT>() {
-            // Integer always exactly representable, trivial implementation.
-            ($lhs as $FloatT).$op(&$rhs)
+macro_rules! int_float_impl_body {
+    ($IntT:ty, $FloatT:ty, $_UnusedT:ty, $lhs:expr, $rhs:expr, $op:ident, $less:expr, $greater:expr, $nan:expr) => {{
+        let lhs = $lhs;
+        let rhs = $rhs;
+
+        // Range in which FloatT is dense in the integers.
+        const FLOAT_DENSE_INT_MIN: $IntT =
+            (0 as $IntT).saturating_sub(1) << <$FloatT>::MANTISSA_DIGITS;
+        const FLOAT_DENSE_INT_MAX: $IntT = 1 << <$FloatT>::MANTISSA_DIGITS;
+
+        // One above IntT::MAX as FloatT. May be infinite.
+        const INT_MAX_POWER_OF_TWO: $IntT = <$IntT>::MAX ^ (<$IntT>::MAX >> 1);
+        const INT_ONE_ABOVE_MAX_AS_FLOAT: $FloatT = 2.0 * (INT_MAX_POWER_OF_TWO as $FloatT);
+
+        if FLOAT_DENSE_INT_MIN <= lhs && lhs <= FLOAT_DENSE_INT_MAX {
+            // lhs is exactly representable as an integer valued float.
+            (lhs as $FloatT).$op(&rhs)
+        } else if INT_ONE_ABOVE_MAX_AS_FLOAT <= rhs {
+            $less
+        } else if <$IntT>::MIN as $FloatT > rhs {
+            $greater
+        } else if rhs.is_nan() {
+            $nan
         } else {
-            // All integers equal or less than FLOAT_DENSE_INT_LIMIT
-            // in magnitude are exactly representable in FloatT, the
-            // next integer isn't.
-            const FLOAT_DENSE_INT_LIMIT: u64 = 1u64 << <$FloatT>::MANTISSA_DIGITS;
-            if $lhs.unsigned_abs() <= FLOAT_DENSE_INT_LIMIT.try_into().unwrap() {
-                // self is exactly representable as an integer valued float.
-                ($lhs as $FloatT).$op(&$rhs)
-            } else if -(<$IntT>::MIN as $FloatT) <= $rhs {
-                $less
-            } else if <$IntT>::MIN as $FloatT > $rhs {
-                $greater
-            } else {
-                // The rounding to integer can't affect the outcome, since we know that
-                // `self` is sufficiently large such that if `other` is close, it must be
-                // an integer.
-                if $rhs.is_nan() {
-                    // NaN is incomparable.
-                    $nan
-                } else {
-                    $lhs.$op(&($rhs as $IntT))
-                }
-            }
+            // The rounding to integer can't affect the outcome, since we know that
+            // `lhs` is sufficiently large such that if `rhs` is close, it must be
+            // an integer.
+            lhs.$op(&(rhs as $IntT))
         }
-    };
+    }};
+}
+
+// Must have IntT <= UintT in width.
+macro_rules! int_uint_impl_body {
+    ($IntT:ty, $UintT:ty, $_UnusedT:ty, $lhs:expr, $rhs:expr, $op:ident, $less:expr, $greater:expr, $nan:expr) => {{
+        let lhs = $lhs;
+        let rhs = $rhs;
+
+        if lhs < 0 {
+            $less
+        } else {
+            (lhs as $UintT).$op(&rhs)
+        }
+    }};
 }
 
 macro_rules! apply_impl_body {
-    ($impl_body:ident, $Lhs:ty, $Rhs:ty) => {
-        impl PartialEq<$Rhs> for RealOrd<$Lhs> {
-            fn eq(&self, other: &$Rhs) -> bool {
-                $impl_body!($Lhs, $Rhs, self.0, *other, eq, false, false, false)
+    ($impl_body:ident, $Lhs:ty, $Rhs:ty, $CommonT:ty) => {
+        impl PartialEq<NumOrd<$Rhs>> for NumOrd<$Lhs> {
+            fn eq(&self, other: &NumOrd<$Rhs>) -> bool {
+                $impl_body!($Lhs, $Rhs, $CommonT, self.0, other.0, eq, false, false, false)
             }
         }
 
-        impl PartialOrd<$Rhs> for RealOrd<$Lhs> {
-            fn partial_cmp(&self, other: &$Rhs) -> Option<Ordering> {
+        impl PartialOrd<NumOrd<$Rhs>> for NumOrd<$Lhs> {
+            fn partial_cmp(&self, other: &NumOrd<$Rhs>) -> Option<Ordering> {
                 $impl_body!(
                     $Lhs,
                     $Rhs,
+                    $CommonT,
                     self.0,
-                    *other,
+                    other.0,
                     partial_cmp,
                     Some(Ordering::Less),
                     Some(Ordering::Greater),
@@ -59,148 +74,192 @@ macro_rules! apply_impl_body {
                 )
             }
 
-            fn lt(&self, other: &$Rhs) -> bool {
-                $impl_body!($Lhs, $Rhs, self.0, *other, lt, true, false, false)
+            fn lt(&self, other: &NumOrd<$Rhs>) -> bool {
+                $impl_body!($Lhs, $Rhs, $CommonT, self.0, other.0, lt, true, false, false)
             }
 
-            fn le(&self, other: &$Rhs) -> bool {
-                $impl_body!($Lhs, $Rhs, self.0, *other, le, true, false, false)
+            fn le(&self, other: &NumOrd<$Rhs>) -> bool {
+                $impl_body!($Lhs, $Rhs, $CommonT, self.0, other.0, le, true, false, false)
             }
 
-            fn gt(&self, other: &$Rhs) -> bool {
-                $impl_body!($Lhs, $Rhs, self.0, *other, gt, false, true, false)
+            fn gt(&self, other: &NumOrd<$Rhs>) -> bool {
+                $impl_body!($Lhs, $Rhs, $CommonT, self.0, other.0, gt, false, true, false)
             }
 
-            fn ge(&self, other: &$Rhs) -> bool {
-                $impl_body!($Lhs, $Rhs, self.0, *other, ge, false, true, false)
+            fn ge(&self, other: &NumOrd<$Rhs>) -> bool {
+                $impl_body!($Lhs, $Rhs, $CommonT, self.0, other.0, ge, false, true, false)
+            }
+        }
+
+        // Reverse implementation.
+        impl PartialEq<NumOrd<$Lhs>> for NumOrd<$Rhs> {
+            fn eq(&self, other: &NumOrd<$Lhs>) -> bool {
+                other == self
+            }
+        }
+
+        impl PartialOrd<NumOrd<$Lhs>> for NumOrd<$Rhs> {
+            fn partial_cmp(&self, other: &NumOrd<$Lhs>) -> Option<Ordering> {
+                other.partial_cmp(self).map(|o| o.reverse())
+            }
+
+            fn lt(&self, other: &NumOrd<$Lhs>) -> bool {
+                other > self
+            }
+
+            fn le(&self, other: &NumOrd<$Lhs>) -> bool {
+                other >= self
+            }
+
+            fn gt(&self, other: &NumOrd<$Lhs>) -> bool {
+                other < self
+            }
+
+            fn ge(&self, other: &NumOrd<$Lhs>) -> bool {
+                other <= self
             }
         }
     };
 }
 
-apply_impl_body!(signed_float_impl_body, i64, f64);
+apply_impl_body!(int_float_impl_body, i64, f32, ());
+apply_impl_body!(int_float_impl_body, i128, f32, ());
+apply_impl_body!(int_float_impl_body, i64, f64, ());
+apply_impl_body!(int_float_impl_body, i128, f64, ());
+apply_impl_body!(int_float_impl_body, u64, f32, ());
+apply_impl_body!(int_float_impl_body, u128, f32, ());
+apply_impl_body!(int_float_impl_body, u64, f64, ());
+apply_impl_body!(int_float_impl_body, u128, f64, ());
 
-pub trait CommonType<T>
+apply_impl_body!(int_uint_impl_body, i8, u128, ());
+apply_impl_body!(int_uint_impl_body, i16, u128, ());
+apply_impl_body!(int_uint_impl_body, i32, u128, ());
+apply_impl_body!(int_uint_impl_body, i64, u128, ());
+apply_impl_body!(int_uint_impl_body, i128, u128, ());
+
+
+pub trait CommonNumType<T>
 where
     Self: Sized,
 {
-    type CommonT: From<T> + From<Self>;
-}
+    type Common: From<T> + From<Self>;
 
-impl<T> CommonType<T> for T {
-    type CommonT = T;
+    fn into_common_type(self) -> Self::Common {
+        self.into()
+    }
 }
 
 macro_rules! impl_common_type {
     ($($T:ty, $U:ty => $C:ty;)*) => {$(
-        impl CommonType<$T> for $U {
-            type CommonT = $C;
-        }
-        impl CommonType<$U> for $T {
-            type CommonT = $C;
+        impl CommonNumType<$T> for $U {
+            type Common = $C;
         }
     )*}
 }
 
 impl_common_type! {
-    u8, i8 => i16;        i8, u16 => i32;     
-    u8, u16 => u16;       i8, i16 => i16;     
-    u8, i16 => i16;       i8, u32 => i64;     
-    u8, u32 => u32;       i8, i32 => i32;     
-    u8, i32 => i32;       i8, u64 => i128;    
-    u8, u64 => u64;       i8, i64 => i64;     
-    u8, i64 => i64;       i8, i128 => i128;   
-    u8, u128 => u128;     i8, f32 => f32;     
-    u8, i128 => i128;     i8, f64 => f64;     
-    u8, f32 => f32;
-    u8, f64 => f64;
-    
-    u16, i16 => i32;      i16, u32 => i64;   
-    u16, u32 => u32;      i16, i32 => i32;   
-    u16, i32 => i32;      i16, u64 => i128;  
-    u16, u64 => u64;      i16, i64 => i64;   
-    u16, i64 => i64;      i16, i128 => i128; 
-    u16, u128 => u128;    i16, f32 => f32;   
-    u16, i128 => i128;    i16, f64 => f64;   
-    u16, f32 => f32;
-    u16, f64 => f64;
-    
-    u32, i32 => i64;      i32, u64 => i128;  
-    u32, u64 => u64;      i32, i64 => i64;   
-    u32, i64 => i64;      i32, i128 => i128; 
-    u32, u128 => u128;    i32, f32 => f64;   
-    u32, i128 => i128;    i32, f64 => f64;   
-    u32, f32 => f64;
-    u32, f64 => f64;
-    
-    u64, i64 => i128;     i64, i128 => i128;
-    u64, u128 => u128;
-    u64, i128 => i128;
-    
-    f32, f64 => f64;
+    // See tools/gen.py.
+      u8,   u8 =>   u8;
+      u8,   i8 =>  i16;   i8,   u8 =>  i16;
+      u8,  u16 =>  u16;  u16,   u8 =>  u16;
+      u8,  i16 =>  i16;  i16,   u8 =>  i16;
+      u8,  u32 =>  u32;  u32,   u8 =>  u32;
+      u8,  i32 =>  i32;  i32,   u8 =>  i32;
+      u8,  u64 =>  u64;  u64,   u8 =>  u64;
+      u8,  i64 =>  i64;  i64,   u8 =>  i64;
+      u8, u128 => u128; u128,   u8 => u128;
+      u8, i128 => i128; i128,   u8 => i128;
+      u8,  f32 =>  f32;  f32,   u8 =>  f32;
+      u8,  f64 =>  f64;  f64,   u8 =>  f64;
+      i8,   i8 =>   i8;
+      i8,  u16 =>  i32;  u16,   i8 =>  i32;
+      i8,  i16 =>  i16;  i16,   i8 =>  i16;
+      i8,  u32 =>  i64;  u32,   i8 =>  i64;
+      i8,  i32 =>  i32;  i32,   i8 =>  i32;
+      i8,  u64 => i128;  u64,   i8 => i128;
+      i8,  i64 =>  i64;  i64,   i8 =>  i64;
+      i8, i128 => i128; i128,   i8 => i128;
+      i8,  f32 =>  f32;  f32,   i8 =>  f32;
+      i8,  f64 =>  f64;  f64,   i8 =>  f64;
+     u16,  u16 =>  u16;
+     u16,  i16 =>  i32;  i16,  u16 =>  i32;
+     u16,  u32 =>  u32;  u32,  u16 =>  u32;
+     u16,  i32 =>  i32;  i32,  u16 =>  i32;
+     u16,  u64 =>  u64;  u64,  u16 =>  u64;
+     u16,  i64 =>  i64;  i64,  u16 =>  i64;
+     u16, u128 => u128; u128,  u16 => u128;
+     u16, i128 => i128; i128,  u16 => i128;
+     u16,  f32 =>  f32;  f32,  u16 =>  f32;
+     u16,  f64 =>  f64;  f64,  u16 =>  f64;
+     i16,  i16 =>  i16;
+     i16,  u32 =>  i64;  u32,  i16 =>  i64;
+     i16,  i32 =>  i32;  i32,  i16 =>  i32;
+     i16,  u64 => i128;  u64,  i16 => i128;
+     i16,  i64 =>  i64;  i64,  i16 =>  i64;
+     i16, i128 => i128; i128,  i16 => i128;
+     i16,  f32 =>  f32;  f32,  i16 =>  f32;
+     i16,  f64 =>  f64;  f64,  i16 =>  f64;
+     u32,  u32 =>  u32;
+     u32,  i32 =>  i64;  i32,  u32 =>  i64;
+     u32,  u64 =>  u64;  u64,  u32 =>  u64;
+     u32,  i64 =>  i64;  i64,  u32 =>  i64;
+     u32, u128 => u128; u128,  u32 => u128;
+     u32, i128 => i128; i128,  u32 => i128;
+     u32,  f32 =>  f64;  f32,  u32 =>  f64;
+     u32,  f64 =>  f64;  f64,  u32 =>  f64;
+     i32,  i32 =>  i32;
+     i32,  u64 => i128;  u64,  i32 => i128;
+     i32,  i64 =>  i64;  i64,  i32 =>  i64;
+     i32, i128 => i128; i128,  i32 => i128;
+     i32,  f32 =>  f64;  f32,  i32 =>  f64;
+     i32,  f64 =>  f64;  f64,  i32 =>  f64;
+     u64,  u64 =>  u64;
+     u64,  i64 => i128;  i64,  u64 => i128;
+     u64, u128 => u128; u128,  u64 => u128;
+     u64, i128 => i128; i128,  u64 => i128;
+     i64,  i64 =>  i64;
+     i64, i128 => i128; i128,  i64 => i128;
+    u128, u128 => u128;
+    i128, i128 => i128;
+     f32,  f32 =>  f32;
+     f32,  f64 =>  f64;  f64,  f32 =>  f64;
+     f64,  f64 =>  f64;
 }
 
+impl<Lhs, Rhs> PartialEq<NumOrd<Rhs>> for NumOrd<Lhs>
+where
+    Lhs: CommonNumType<Rhs> + Copy,
+    Rhs: CommonNumType<Lhs> + Copy,
+    <Lhs as CommonNumType<Rhs>>::Common: PartialEq<<Rhs as CommonNumType<Lhs>>::Common>,
+{
+    fn eq(&self, other: &NumOrd<Rhs>) -> bool {
+        self.0.into_common_type() == other.0.into_common_type()
+    }
+}
 
+impl<Lhs, Rhs> PartialOrd<NumOrd<Rhs>> for NumOrd<Lhs>
+where
+    Lhs: CommonNumType<Rhs> + Copy,
+    Rhs: CommonNumType<Lhs> + Copy,
+    <Lhs as CommonNumType<Rhs>>::Common: PartialOrd<<Rhs as CommonNumType<Lhs>>::Common>,
+{
+    fn partial_cmp(&self, other: &NumOrd<Rhs>) -> Option<Ordering> {
+        self.0.into_common_type().partial_cmp(&other.0.into_common_type())
+    }
 
+    fn lt(&self, other: &NumOrd<Rhs>) -> bool {
+        self.0.into_common_type() < other.0.into_common_type()
+    }
 
-// impl PartialOrd<$Rhs> for RealOrd<$Lhs> {
-//     fn partial_cmp(&self, other: &$Rhs) -> Option<Ordering> {
-//         $impl_body!(
-//             $Lhs,
-//             $Rhs,
-//             self.0,
-//             *other,
-//             partial_cmp,
-//             Some(Ordering::Less),
-//             Some(Ordering::Greater),
-//             None
-//         )
-//     }
+    fn le(&self, other: &NumOrd<Rhs>) -> bool {
+        self.0.into_common_type() <= other.0.into_common_type()
+    }
 
-//     fn lt(&self, other: &$Rhs) -> bool {
-//         $impl_body!($Lhs, $Rhs, self.0, *other, lt, true, false, false)
-//     }
+    fn gt(&self, other: &NumOrd<Rhs>) -> bool {
+        self.0.into_common_type() > other.0.into_common_type()
+    }
 
-//     fn le(&self, other: &$Rhs) -> bool {
-//         $impl_body!($Lhs, $Rhs, self.0, *other, le, true, false, false)
-//     }
-
-//     fn gt(&self, other: &$Rhs) -> bool {
-//         $impl_body!($Lhs, $Rhs, self.0, *other, gt, false, true, false)
-//     }
-
-//     fn ge(&self, other: &$Rhs) -> bool {
-//         $impl_body!($Lhs, $Rhs, self.0, *other, ge, false, true, false)
-//     }
-// }
-
-
-
-
-
-
-
-
-
-// NO COMMON TYPE i8 u128
-// NO COMMON TYPE i16 u128
-// NO COMMON TYPE i32 u128
-// NO COMMON TYPE i64 u128
-// NO COMMON TYPE i128 u128
-
-// NO COMMON TYPE i64 f32
-// NO COMMON TYPE i64 f64
-// NO COMMON TYPE i128 f32
-// NO COMMON TYPE i128 f64
-// NO COMMON TYPE u64 f32
-// NO COMMON TYPE u64 f64
-// NO COMMON TYPE u128 f32
-// NO COMMON TYPE u128 f64
-
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn ge(&self, other: &NumOrd<Rhs>) -> bool {
+        self.0.into_common_type() >= other.0.into_common_type()
     }
 }
